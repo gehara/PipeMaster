262bc6766ef428dcde75ef22cb7d49dd9d44e85c
gehara <marcelo.gehara@gmail.com>
2017-09-16 12:14:38 -0400

    observed moments

--- R/observed.sumstat.R
+++ R/observed.sumstat.R
@@ -1,7 +1,7 @@
-observed.sumstat<-function(model,path.to.fasta,fasta.files=list.files(),overall.SS=T,perpop.SS=T){
+observed.sumstat<-function(model,path.to.fasta,fasta.files=list.files(),overall.SS=T,perpop.SS=T,get.moments=F){
 
   fasta.files<-fasta.files<-fasta.files[grep(".fas",fasta.files)]
-  
+
   fasta2ms(path.to.fasta,fasta.files,write.file=T)
   # get population structure
   if(perpop.SS==T){
@@ -14,11 +14,23 @@ observed.sumstat<-function(model,path.to.fasta,fasta.files=list.files(),overall.
 
   setwd(path.to.fasta)
   if(perpop.SS==T){
-    write.table(t(NAMES),file="ObservedSS.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+      write.table(t(NAMES),file ="observed_popstats_mean.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+      if(get.moments==T){
+        write.table(t(NAMES),file = "observed_popstats_var.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+        write.table(t(NAMES),file = "observed_popstats_kur.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+        write.table(t(NAMES),file = "observed_popstats_skew.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+      }
     }
-  if(overall.SS==T){
-    write.table(t(overall.NAMES),file="ObservedoverallSS.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+    if(overall.SS==T){
+      write.table(t(overall.NAMES),file = "observed_overallstats_mean.txt",
+                                                quote=F,row.names=F, col.names = F,sep="\t",append=F)
+      if(get.moments==T){
+        write.table(t(overall.NAMES),file = "observed_overallstats_var.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+        write.table(t(overall.NAMES),file = "observed_overallstats_kur.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+        write.table(t(overall.NAMES),file = "observed_overallstats_skew.txt",quote=F,row.names=F, col.names = F,sep="\t",append=F)
+      }
     }
+
 
   ss<-list(NULL)
   OA.ss<-list(NULL)
@@ -50,12 +62,76 @@ observed.sumstat<-function(model,path.to.fasta,fasta.files=list.files(),overall.
       ss[[u]]<-cbind(s.sites,pi.within,Hap.div,Taj.D,Fu.Li.D,Fu.Li.F,Hap.Fst,nuc.Fst)
     }
   }
+
   if(perpop.SS==T){
-    ss<-colMeans(do.call(rbind, ss), na.rm = TRUE)
-    write.table(ss,file="ObservedSS.txt",quote=F,row.names=F, col.names = F, append=T,sep="\t")
+    SS<-NULL
+    kur<-NULL
+    vari<-NULL
+    skew<-NULL
+    for(jj in 1:sim.block.size){
+      x<-NULL
+      for(ii in 1:nrow(model$loci)){
+        x<-rbind(x,ss[[ii]][jj,])
+      }
+      SS<-rbind(SS,colMeans(x, na.rm=T))
+      if(get.moments==T){
+        vari<-rbind(vari,diag(var(x, na.rm=T)))
+        kk<-NULL
+        sk<-NULL
+        for(uu in 1:ncol(x)){
+          s<-skewness(x[,uu],na.rm=T)
+          sk<-c(sk,s)
+          k<-kurtosis(x[,uu],na.rm=T)
+          kk<-c(kk,k)
+        }
+        kur<-rbind(kur,kk)
+        skew<-rbind(skew,sk)
+      }
+    }
+    # write outputs
+    write.table(SS,file="observed_popstats_mean.txt",quote=F,row.names=F, col.names = F, append=T,sep="\t")
+    if(get.moments==T){
+      write.table(vari,file="observed_popstats_var.txt",quote=F,row.names=F, col.names = F, append=T,sep="\t")
+      write.table(kur,file="observed_popstats_kur.txt",quote=F,row.names=F, col.names = F, append=T,sep="\t")
+      write.table(skew,file="observed_popstats_skew.txt",quote=F,row.names=F, col.names = F, append=T,sep="\t")
+    }
   }
   if(overall.SS==T){
-    OA.ss<-colMeans(do.call(rbind, OA.ss), na.rm = TRUE)
-    write.table(OA.ss,file="ObservedoverallSS.txt",quote=F,row.names=F, col.names = F, append=T,sep="\t")
+    SS<-NULL
+    kur<-NULL
+    vari<-NULL
+    skew<-NULL
+    for(jj in 1:sim.block.size){
+      x<-NULL
+      for(ii in 1:nrow(model$loci)){
+        x<-rbind(x,OA.ss[[ii]][jj,])
+      }
+      SS<-rbind(SS,colMeans(x, na.rm=T))
+      if(get.moments==T){
+        vari<-rbind(vari,diag(var(x, na.rm=T)))
+        kk<-NULL
+        sk<-NULL
+        for(uu in 1:ncol(x)){
+          s<-skewness(x[,uu],na.rm=T)
+          sk<-c(sk,s)
+          k<-kurtosis(x[,uu],na.rm=T)
+          kk<-c(kk,k)
+        }
+        kur<-rbind(kur,kk)
+        skew<-rbind(skew,sk)
+      }
+    }
+
+
+    write.table(SS,file="observed_overallstats_mean.txt",
+                              quote=F,row.names=F, col.names = F, append=T,sep="\t")
+    if(get.moments==T){
+      write.table(vari,file="observed_overallstats_var.txt",
+                                  quote=F,row.names=F, col.names = F, append=T,sep="\t")
+      write.table(kur,file="observed_overallstats_kur.txt",
+                                 quote=F,row.names=F, col.names = F, append=T,sep="\t")
+      write.table(skew,file="observed_overallstats_skew.txt",
+                                 quote=F,row.names=F, col.names = F, append=T,sep="\t")
+    }
   }
 }
