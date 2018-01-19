9c6a4679d96d54253c132e5c611d79200d2ea2e4
gehara <marcelo.gehara@gmail.com>
2018-01-19 10:29:10 -0200

    nexgen observed sumstat names

--- R/observed.snp.sumstat.R
+++ R/observed.snp.sumstat.R
@@ -1,5 +1,18 @@
 
-observed.snp.sumstat<-function(path.to.fasta,pop.assign,moments=F,msABC.call){
+observed.snp.sumstat<-function(model,path.to.fasta,pop.assign,moments=F,msABC.call){
+
+
+    com<-ms.commander.snp(model,use.alpha=F,
+                          msABC=msABC.call)
+    x<-strsplit(system(com[[1]],intern=T),"\t")
+    nam<-x[1]
+    mean_nam<-paste(nam[[1]],"_mean",sep="")
+    if(moments==T){
+      kur_nam<-paste(nam[[1]],"_kur",sep="")
+      skew_nam<-paste(nam[[1]],"_skew",sep="")
+      var_nam<-paste(nam[[1]],"_var",sep="")
+    }
+
   setwd(path.to.fasta)
   fasta.files<-list.files()
   fasta.files<-fasta.files[grep(".fa",fasta.files,fixed=T)]
@@ -17,11 +30,16 @@ observed.snp.sumstat<-function(path.to.fasta,pop.assign,moments=F,msABC.call){
   observed<-matrix(unlist(observed), ncol = length(observed[[1]]), byrow = TRUE)
 
   mean<-colMeans(observed,na.rm = T)
+  names(mean)<-mean_nam
   if(moments==T){
   kur<-apply(observed,2,kurtosis, na.rm=T)
+  names(kur)<-kur_nam
   skew<-apply(observed,2,skewness, na.rm=T)
+  names(skew)<-skew_nam
   var<-apply(observed,2,var, na.rm=T)
+  names(var)<-var_nam
   }
+
   observed<-list(mean,NULL,NULL,NULL)
   names(observed)<-c("mean","variance","kurtosis","skewness")
   if(moments==T){
@@ -31,6 +49,3 @@ observed.snp.sumstat<-function(path.to.fasta,pop.assign,moments=F,msABC.call){
   }
   return(observed)
   }
-
-
-
