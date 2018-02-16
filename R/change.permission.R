7270c3484d9110e91f26fcf508adcef6ede069dc
gehara <marcelo.gehara@gmail.com>
2018-02-16 14:18:16 -0500

    permission msABC

--- R/change.permission.R
+++ R/change.permission.R
@@ -2,7 +2,7 @@
 change.permission<-function(mode="0777"){
 
 pack<-find.package("PipeMaster")
-if(Sys.info()[1]=="Linux"){
+if(Sys.info()[1]=="Darwin"){
   msABC.call<-paste(pack,"/msABCmac",sep="")
 }
 if(Sys.info()[1]=="Linux"){
