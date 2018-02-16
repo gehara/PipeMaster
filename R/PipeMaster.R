312e7f207ef634a708eafc0c59678c0f528b1ed8
gehara <marcelo.gehara@gmail.com>
2018-02-16 14:22:48 -0500

    msABC permission

---
+++ R/PipeMaster.R
@@ -0,0 +1,8 @@
+#' PipeMaster: A package a package to build and simulate coalescent models
+#'
+#' @section PipeMaster functions:
+#' observed.snp.sumstat
+#'
+#' @docType package
+#' @name PipeMaster
+NULL
--- R/change.permission.R
+++ R/change.permission.R
@@ -1,4 +1,4 @@
-
+#' @export
 change.permission<-function(mode="0777"){
 
 pack<-find.package("PipeMaster")
