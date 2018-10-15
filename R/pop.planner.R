793226b4a55d8f7a5597e27d3b02740836577850
gehara <marcelo.gehara@gmail.com>
2018-10-15 13:36:58 -0400

    documentation update

--- R/pop.planner.R
+++ R/pop.planner.R
@@ -1,3 +1,7 @@
+#' Calls PopPlanner, a java aplication with GUI to build ms population models generating the coresponding ms string.
+#' @description When you run this function PopPlanner will pop up. PopPlanner has a easy to use interface to build a diversification models.
+#'            The resulting ms string will be generated in the botton of the PopPlanner window. This can be used as an input to the main.menu function.
+#' @export
 PopPlanner<-function(){
   pack <- find.package("PipeMaster")
   Sys.chmod(paths=paste(pack,"/PopPlanner.jar"), mode = 7777, use_umask = TRUE)
