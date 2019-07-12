3f7ae4abefdb902d9630b6f6fb47631a90cc25ed
gehara <marcelo.gehara@gmail.com>
2019-07-12 10:44:27 -0400

    code organization update

--- R/get.msABC.R
+++ R/get.msABC.R
@@ -12,23 +12,3 @@ get.msABC<-function(){
   return(msABC.call)
   }
 
-# internal function to generate the locus file
-get.locfile<-function(model){
-  locfile<-NULL
-  for(i in 1:nrow(model$loci)){
-    for(j in 1:model$I[1,3]){
-      locfile<-rbind(locfile,c(model$I[i,1],model$I[i,j+3],j,model$loci[i,2],model$loci[i,4],0))
-    }
-    colnames(locfile)<-c("id","n","pop","length","mu","rec")
-  }
-  return(locfile)
-}
-
-# internal function to generate the locus file
-sample.mu.rates<-function(model){
-  MEAN <- runif(1, as.numeric(model$loci[1,4]), as.numeric(model$loci[1,5]))
-  SD <- runif(1, as.numeric(model$loci[1,4]), as.numeric(model$loci[1,5]))
-  rates<-rtnorm(nrow(model$loci),MEAN,SD,0)
-  rates<-rep(rates, each=as.numeric(model$I[1,3]))
-  return(list(rates,c(MEAN,SD)))
-    }
