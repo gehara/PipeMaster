b4438efebaa6c6efe5d572f3b1da683878b30da8
gehara <marcelo.gehara@gmail.com>
2022-05-03 15:28:07 -0400

    parallel msABC.sanger

--- R/sim.msABC.R
+++ R/sim.msABC.R
@@ -45,8 +45,8 @@ sim.msABC.sanger <- function(model,use.alpha=F, nsim.blocks=5, path=getwd(), app
 
   dput(model, ".model")
   dput(list(msABC.call, block.size, use.alpha), ".objects")
-  file.copy(paste(system.file(package="PipeMaster"),"/run.msABC.R", sep=""), paste(path,"/.msABC", sep=""), overwrite = T)
-  Sys.chmod(paths = paste(path,"/.msABC", sep=""), mode = 7777, use_umask = TRUE)
+  file.copy(paste(system.file(package="PipeMaster"),"/run.msABC.R", sep=""), paste(path,"/.msABC.R", sep=""), overwrite = T)
+  Sys.chmod(paths = paste(path,"/.msABC.R", sep=""), mode = 0777, use_umask = TRUE)
 
   total.sims<-0
   for(k in 1:nsim.blocks){
@@ -55,7 +55,7 @@ sim.msABC.sanger <- function(model,use.alpha=F, nsim.blocks=5, path=getwd(), app
 
   write(0,".log")
   for(j in 1:ncores){
-    system(paste("Rscript .msABC",j), wait = F)
+    system(paste("Rscript .msABC.R",j), wait = F)
   }
 
   l<-"0"
